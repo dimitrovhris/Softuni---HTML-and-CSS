6eaf4f83ee8245a6b8127eb55a538c19469d7e4c
dimitrovhris <hrisit12@gmail.com>
2023-02-23 22:13:56 +0200

    Solved a problem.

---
+++ JavaScript/Syntax_Fundamentals/Ex06LargestNumber.js
@@ -0,0 +1,11 @@
+function solve(num1, num2, num3){
+    let largestNumber;
+    if(num1 > num2 && num1 > num3){
+        largestNumber = num1;
+    } else if(num2 > num1 && num2 > num3){
+        largestNumber = num2;
+    } else if(num3 > num1 && num3 > num2){
+        largestNumber = num3;
+    }
+    console.log(`The largest number is ${largestNumber}.`);
+}
