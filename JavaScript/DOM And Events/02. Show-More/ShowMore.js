6c07f66770942fa1f8e3fe0bd48a5dfd6330919b
dimitrovhris <hrisit12@gmail.com>
2023-03-22 19:23:46 +0200

    Solved a problem.

--- JavaScript/DOM And Events/02. Show-More/ShowMore.js
+++ JavaScript/DOM And Events/02. Show-More/ShowMore.js
@@ -1,3 +1,6 @@
 function showText() {
-    // TODO
+    let link = document.getElementById(`more`);
+    link.style.display = `none`;
+    let text = document.getElementById(`text`);
+    text.style.display = `inline`;
 }
