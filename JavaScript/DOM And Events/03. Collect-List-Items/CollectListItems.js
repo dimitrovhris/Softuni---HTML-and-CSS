1c0b38f0d7d6bba52826c6ec40f0b5f9317239f7
dimitrovhris <hrisit12@gmail.com>
2023-03-22 19:39:43 +0200

    Solved a problem.

--- JavaScript/DOM And Events/03. Collect-List-Items/CollectListItems.js
+++ JavaScript/DOM And Events/03. Collect-List-Items/CollectListItems.js
@@ -1,3 +1,8 @@
 function extractText() {
-    // TODO
+    let items = document.getElementsByTagName(`li`);
+    let liArray = Array.from(items);
+    let textArea = document.getElementById(`result`);
+    for (const item of liArray) {
+        textArea.value += item.textContent + `\n`;
+    }
 }
