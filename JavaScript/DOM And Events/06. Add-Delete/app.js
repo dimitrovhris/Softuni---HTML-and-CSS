f74eb1c74fe5e8ba6b2c786bb9823f1c821e03da
dimitrovhris <hrisit12@gmail.com>
2023-03-23 21:49:49 +0200

    Solved a problem.

--- JavaScript/DOM And Events/06. Add-Delete/app.js
+++ JavaScript/DOM And Events/06. Add-Delete/app.js
@@ -1,3 +1,17 @@
 function addItem() {
-    //TODO...
+    const ulContainer = document.getElementById(`items`);
+    const input = document.getElementById(`newItemText`);
+    const newLi = document.createElement(`li`);
+    const newAnchor = document.createElement(`a`);
+    newLi.textContent = input.value;
+    newAnchor.textContent = `[Delete]`;
+    newAnchor.href = `#`;
+    newLi.appendChild(newAnchor);
+    ulContainer.appendChild(newLi);
+    newAnchor.addEventListener(`click`, deleteHandler);
+    input.value = ``;
+    function deleteHandler(e){
+        const liItem = e.currentTarget.parentElement;
+        liItem.remove();
+    }
 }
