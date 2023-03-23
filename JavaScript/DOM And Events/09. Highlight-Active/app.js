c0ab6c7a62a97f293cc9b53238002ad426256261
dimitrovhris <hrisit12@gmail.com>
2023-03-23 23:19:14 +0200

    Solved a problem.

--- JavaScript/DOM And Events/09. Highlight-Active/app.js
+++ JavaScript/DOM And Events/09. Highlight-Active/app.js
@@ -1,3 +1,14 @@
 function focused() {
-    console.log('TODO:...');
+    let inputs = document.getElementsByTagName('input');
+    for (const input of inputs) {
+        input.addEventListener('focus', focus);
+        input.addEventListener(`blur`, blur);
+    }
+    function focus(e){
+        let currentInput = e.target.parentElement;
+        currentInput.className = `focused`;
+    }
+    function blur(e){
+        e.target.parentElement.className = ``;
+    }
 }
