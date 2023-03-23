06b008b79872086c1c38c040732a26dc12cbfc87
dimitrovhris <hrisit12@gmail.com>
2023-03-23 22:12:27 +0200

    Solved a problem.

--- JavaScript/DOM And Events/07. Colorized-Table/ColorizeTable.js
+++ JavaScript/DOM And Events/07. Colorized-Table/ColorizeTable.js
@@ -1,3 +1,8 @@
 function colorize() {
-    // TODO
+    let rows = document.querySelectorAll(`table tbody tr:nth-child(even)`);
+
+    for (const row of rows) {
+        let childrenArray = Array.from(row.children);
+        childrenArray.forEach(e => e.style = `background-color: teal`);
+    }
 }
