872000f36107328a773b5aae99ae9097e9467736
dimitrovhris <hrisit12@gmail.com>
2023-03-25 00:04:43 +0200

    Solved a problem.

--- JavaScript/DOM And Events/Exercise/04. Locked-Profile/app.js
+++ JavaScript/DOM And Events/Exercise/04. Locked-Profile/app.js
@@ -1,3 +1,20 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let profiles = Array.from(document.getElementsByClassName('profile'));
+    for (const profile of profiles) {
+        let button = profile.getElementsByTagName('button')[0];
+        button.addEventListener('click', showInfo);
+        function showInfo(e) {
+
+            let radioButtonLock = e.target.parentElement.getElementsByTagName('input')[0];
+            if (radioButtonLock.checked !== true) {
+                if (button.textContent !== 'Hide it') {
+                    profile.getElementsByTagName('div')[0].style = `display:block`;
+                    button.textContent = 'Hide it';
+                } else{
+                    profile.getElementsByTagName('div')[0].style = `display:none`;
+                    button.textContent = 'Show more';
+                }
+            }
+        }
+    }
 }
