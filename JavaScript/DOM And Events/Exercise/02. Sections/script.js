6e6d4b7ee29a10bbe75cf9c075b8a28672d5cb7c
dimitrovhris <hrisit12@gmail.com>
2023-03-24 22:24:12 +0200

    Solved a problem.

--- JavaScript/DOM And Events/Exercise/02. Sections/script.js
+++ JavaScript/DOM And Events/Exercise/02. Sections/script.js
@@ -1,3 +1,15 @@
 function create(words) {
-   console.log('TODO:...');
+   for (const word of words) {
+      let div = document.createElement('div');
+      let paragraph = document.createElement('p');
+      paragraph.textContent = word;
+      paragraph.style = 'display:none';
+      div.appendChild(paragraph);
+      div.addEventListener('click', visible);
+      function visible(e){
+         e.target.firstChild.style = "display: block";
+      }
+      document.getElementById('content').appendChild(div);
+   }
+   
 }
