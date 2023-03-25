9fc4d3183c086dd7501074d6ed9025b5869a8a6e
dimitrovhris <hrisit12@gmail.com>
2023-03-25 11:34:19 +0200

    Solved a problem.

--- JavaScript/DOM And Events/Exercise/06. Table - Search Engine/solution.js
+++ JavaScript/DOM And Events/Exercise/06. Table - Search Engine/solution.js
@@ -2,7 +2,17 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
-
+         let text = document.getElementById(`searchField`).value;
+         document.getElementById(`searchField`).value = '';
+         let rows = Array.from(document.querySelectorAll('tbody tr'));
+         rows.forEach(row => row.setAttribute('class', ''));
+         for (const row of rows) {
+            let cells = Array.from(row.children);
+            for (const cell of cells) {
+               if(cell.textContent.includes(text)){
+                  row.setAttribute('class', 'select');
+               }
+            }
+         }
    }
 }
