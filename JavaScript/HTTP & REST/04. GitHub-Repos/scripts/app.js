41bd67e7f8616077d8dd47074cdf3ed19a3de4a2
dimitrovhris <hrisit12@gmail.com>
2023-04-01 19:37:03 +0300

    Solved a problem.

---
+++ JavaScript/HTTP & REST/04. GitHub-Repos/scripts/app.js
@@ -0,0 +1,9 @@
+function loadRepos() {
+   const BASE_URL = 'https://api.github.com/users/testnakov/repos';
+   const resultContainer = document.getElementById('res');
+   fetch(BASE_URL, {method: 'GET'})
+      .then((res) => res.text())
+      .then((data) => {
+         resultContainer.textContent = data;
+      })
+}
