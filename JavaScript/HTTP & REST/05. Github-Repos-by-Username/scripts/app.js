5f5cc5ed018430d9d74ed3f13fdaab4f4ab9e798
dimitrovhris <hrisit12@gmail.com>
2023-04-01 21:46:13 +0300

    Solved a problem.

--- JavaScript/HTTP & REST/05. Github-Repos-by-Username/scripts/app.js
+++ JavaScript/HTTP & REST/05. Github-Repos-by-Username/scripts/app.js
@@ -1,3 +1,21 @@
 function loadRepos() {
-	console.log("TODO...");
+	const BASE_URL = 'https://api.github.com/users/';
+	let username = document.getElementById('username').value;
+	console.log(username);
+	let ul = document.getElementById('repos');
+	fetch(BASE_URL + '' + username + '/repos')
+	.then((res) => res.json())
+	.then((repos)=> {
+		for (const repo of repos) {
+			let li = document.createElement('li');
+			let a = document.createElement('a');
+			let link = repo.html_url;
+			a.href = link;
+			a.textContent = repo.full_name;
+			li.appendChild(a);
+			ul.appendChild(li);
+		}
+	}
+	)
+
 }
