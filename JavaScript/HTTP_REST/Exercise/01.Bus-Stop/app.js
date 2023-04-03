a12c136ec605770083584a8b6e803b75d4f91ac3
dimitrovhris <hrisit12@gmail.com>
2023-04-03 20:29:58 +0300

    Solved a problem.

--- JavaScript/HTTP_REST/Exercise/01.Bus-Stop/app.js
+++ JavaScript/HTTP_REST/Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,28 @@
 function getInfo() {
-    // TODO:
-}
+    const list = document.getElementById('buses');
+    const stop = document.getElementById('stopId');
+    const stopID = stop.value;
+    const stopNameContainer = document.getElementById('stopName');
+    const busesContainer = document.getElementById('buses');
+    const BASE_URL = 'http://localhost:3030/jsonstore/bus/businfo/';
+    fetch(BASE_URL + stopID)
+    .then((response) =>
+        response.json())
+    .then((busInfo) => {
+        console.log(busInfo);
+        const {buses, name} = busInfo;
+        stopNameContainer.textContent = name;
+        for (const bus in buses) {
+            let value = `Bus ${bus} arrives in ${buses[bus]} minutes`;
+            let li = document.createElement('li');
+            li.textContent = value;
+            list.appendChild(li);
+        }
+    }
+    )
+    .catch(() => {
+        stopNameContainer.textContent = 'Error';
+    })
+    list.innerHTML = '';
+    stop.value =  '';
+} 
