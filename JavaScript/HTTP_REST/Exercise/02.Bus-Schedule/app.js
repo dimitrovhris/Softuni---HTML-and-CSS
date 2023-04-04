c2c14df2dac36bd3d027feaaf92c3c7dc410028c
dimitrovhris <hrisit12@gmail.com>
2023-04-04 17:08:24 +0300

    Solved a problem.

--- JavaScript/HTTP_REST/Exercise/02.Bus-Schedule/app.js
+++ JavaScript/HTTP_REST/Exercise/02.Bus-Schedule/app.js
@@ -1,10 +1,42 @@
 function solve() {
+    const BASE_URL = 'http://localhost:3030/jsonstore/bus/schedule/';
+    let nextStop = 'depot';
+    let currentStop = 'none';
+    const infoContainer = document.getElementById('info');
+    const buttonDepart = document.getElementById('depart');
+    const buttonArrive = document.getElementById('arrive');
     function depart() {
-        // TODO: 
+        buttonDepart.disabled = 'true';
+        buttonArrive.removeAttribute('disabled');
+        fetch(BASE_URL + nextStop)
+        .then((response) => response.json())
+        
+        .then((stopData) => {
+            const {name, next} = stopData;
+            console.log(stopData);
+            infoContainer.textContent = `Next stop ${name}`;
+            currentStop = name;
+            nextStop = next;
+        })
+        .catch((err) => {
+            infoContainer.textContent = "Not Connected";
+        })
+
+        
+
     }
 
     async function arrive() {
-        // TODO:
+        try{
+            buttonDepart.removeAttribute('disabled');
+            buttonArrive.disabled = 'true';
+            infoContainer.textContent = `Arriving at ${currentStop}`;
+            nextStop = next;
+        }
+        
+        catch{
+            infoContainer.textContent = "Not Connected";
+        }
     }
 
     return {
